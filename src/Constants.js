65721ea105706d4a7ec72c393865107df153c9bf
duliwencynthia <49981557+duliwencynthia@users.noreply.github.com>
2022-05-02 22:41:04 -0400

    Add files via upload

---
+++ src/Constants.js
@@ -0,0 +1,56 @@
+// SERVER
+export const HOST_URL = "https://www.feldsparhomes.com/";
+export const BASE_URL = "https://www.feldsparhomes.com:8001/";
+export const BASE_URL_AUTH = "https://www.feldsparhomes.com:8000/";
+export const DEFAULT_WALL_MAP = "5f0dde8ee5148d0ef82ff069";
+export const DEFAULT_FLOOR_MAP = "5f0dddf4e5148d0ef82ff066";
+
+// DEV-LOCAL
+// export const HOST_URL = "http://localhost:3000/";
+// export const BASE_URL = "http://localhost:8001/";
+// export const BASE_URL_AUTH = "http://localhost:8000/";
+// export const DEFAULT_WALL_MAP = "5f07f740b5991623548246d7"
+// export const DEFAULT_FLOOR_MAP = "5ef849c28188602a98c95524"
+
+// COMMON-----------------------------------------------------------
+export const ASSETS = "assets/";
+export const MODELS = "models/";
+export const FLOOR_MODELS = "floorModel/"
+export const TEXTURES = "textures/";
+export const RETRIEVE = "retrieve/";
+export const USERS = "users/";
+export const RESOURCES = "resources/";
+export const FIND = "find/";
+export const VIEW_MODEL = "view/"
+
+export const REGISTER = "register";
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const UPLOAD_ASSET = "uploadAsset"
+export const UPDATE_ASSET_ENTRY = "updateAssetEntry"
+
+export const SOFA_CATEGORY = "sofa";
+export const CHAIR_CATEGORY = "chair";
+export const BED_CATEGORY = "bed";
+export const ARCH_CATEGORY = "architectural";
+export const MISC_CATEGORY = "misc";
+export const KITCHEN_CATEGORY = "kitchen";
+export const RUG_CATEGORY = "rug";
+export const TABLE_CATEGORY = "table";
+export const CABINET_CATEGORY = "cabinet";
+export const CURTAIN_CATEGORY = "curtain";
+export const LIGHT_CATEGORY = "light";
+export const BATH_CATEGORY = "bath";
+export const FLOOR_CATEGORY = "floor";
+export const FLOOR_WOOD_CATEGORY = "floorWood";
+export const FLOOR_MARBLE_CATEGORY = "floorMarble";
+export const FLOOR_TILE_CATEGORY = "floorTile";
+export const WALL_CATEGORY = "wall";
+export const WALL_SOLID_CATEGORY = "solid";
+export const WALL_TILE_CATEGORY = "wallTile";
+export const GET_FREE_RESOURCES = "getFreeResources";
+export const GET_RESOURCES = "getResources"
+
+export const REMOVE_MODEL = "removeModel"
+export const RETRIEVE_MODEL = "retrieve"
+export const UPDATE_MODEL = "update"
---
+++ src/LuminescenceStore.js
@@ -0,0 +1,113 @@
+import {
+  observable,
+  action,
+  computed
+} from "mobx";
+
+class LuminescenceStore {
+  @observable loggedIn = false;
+  @observable username = "";
+  @observable accessToken = "";
+  @observable refreshToken = "";
+  @observable loginModal = false;
+  @observable logoutModal = false;
+  @observable infoModal = false;
+  @observable addClickListener = false;
+  @observable saveFileModal = false;
+  @observable loadFileModal = false;
+
+
+  //LoggedIn---------------------------------------------
+  @action setLoggedIn = (login) => {
+    this.loggedIn = login
+  }
+
+  @computed get getLoggedIn() {
+    return this.loggedIn
+  }
+
+  //Username--------------------------------------------
+  @action setUsername = (name) => {
+    this.username = name
+  }
+
+  @computed get getUsername() {
+    return this.username
+  }
+
+  //AccessToken--------------------------------------------
+  @action setAccessToken = (accessToken) => {
+    this.accessToken = accessToken
+  }
+
+  @computed get getAccessToken() {
+    return this.accessToken
+  }
+
+  //RefreshToken--------------------------------------------
+  @action setRefreshToken = (token) => {
+    this.refreshToken = token
+  }
+
+  @computed get getRefreshToken() {
+    return this.refreshToken
+  }
+
+  //LoginModal---------------------------------------------
+  @action setLoginModal = (show) => {
+    this.loginModal = show
+  }
+
+  @computed get showLoginModal() {
+    return this.loginModal
+  }
+
+  //LogoutModal---------------------------------------------
+  @action setLogoutModal = (show) => {
+    this.logoutModal = show
+  }
+
+  @computed get showLogoutModal() {
+    return this.logoutModal
+  }
+
+  //InfoModal---------------------------------------------
+  @action setInfoModal = (show) => {
+    this.infoModal = show
+  }
+
+  @computed get showInfoModal() {
+    return this.infoModal
+  }
+
+  //AddClickListener---------------------------------------------
+  @action setClickListener = (bool) => {
+    this.addClickListener = bool
+  }
+
+  @computed get getClickListener() {
+    return this.addClickListener
+  }
+
+  //Save File Modal---------------------------------------------
+  @action setSaveFileModal = (bool) => {
+    this.saveFileModal = bool
+  }
+
+  @computed get showSaveFileModal() {
+    return this.saveFileModal
+  }
+
+  //Load File Modal---------------------------------------------
+  @action setLoadFileModal = (bool) => {
+    this.loadFileModal = bool
+  }
+
+  @computed get showLoadFileModal() {
+    return this.loadFileModal
+  }
+
+}
+
+var store = new LuminescenceStore();
+export default store;
